7e193ed1f5c5a8ed9d61e0e376e7c2afd57467ef
mei <meiapr10@gmail.com>
2022-12-24 18:05:31 +0530

    feature 2

--- src/main/java/com/cart/pages/cartPage.java
+++ src/main/java/com/cart/pages/cartPage.java
@@ -3,16 +3,15 @@ package com.cart.pages;
 import org.openqa.selenium.By;
 
 public class cartPage {
-
 	private By cart = By.id("cart");
-
 	public void addCart() {
 		System.out.println("page actions is done");
 	}
-
 	public static void main(String args[]) {
 		String s1 = "feature 1";
 		System.out.println(s1);
+		
+		String s2 = "feature 2";
+		System.out.println(s2);
 	}
-
 }
