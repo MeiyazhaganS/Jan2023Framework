d4e5f2e941ef8f9b65ac3bbaa8b944258f61bb07
MeiyazhaganS <120730777+MeiyazhaganS@users.noreply.github.com>
2022-12-25 18:54:40 +0530

    Create Orderpage.java

---
+++ src/main/java/com/cart/pages/Orderpage.java
@@ -0,0 +1,15 @@
+package com.cart.pages;
+
+public class Orderpage {
+
+  	public void getOrderId() {
+		System.out.println("order Id 1234...");
+	}
+
+	public void makeOrder() {
+		System.out.println("order page...");
+	}
+	public static void main(String args[]) {
+		
+	}
+}
