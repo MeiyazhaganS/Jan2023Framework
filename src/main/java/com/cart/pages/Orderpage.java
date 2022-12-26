9cf36cf32475af2c8091e8911e5cfd0931e718cc
mei <meiapr10@gmail.com>
2022-12-26 18:04:30 +0530

    code in stash

--- src/main/java/com/cart/pages/Orderpage.java
+++ src/main/java/com/cart/pages/Orderpage.java
@@ -5,7 +5,6 @@ public class Orderpage {
 
 	public void makeOrder() {
 		System.out.println("order page...");
-		System.out.println("this is my order...");
 	}public static void main(String args[]) {
 		
 	}
