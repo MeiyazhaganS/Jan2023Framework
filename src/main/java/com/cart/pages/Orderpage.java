60abd4244e81b71f391f875f4f41968dfc1adfb8
mei <meiapr10@gmail.com>
2022-12-26 15:51:10 +0530

    latest order page

---
+++ src/main/java/com/cart/pages/Orderpage.java
@@ -0,0 +1,12 @@
+package com.cart.pages;
+
+public class Orderpage {
+
+	public void makeOrder() {
+		System.out.println("order page...");
+		System.out.println("this is my order...");
+	}
+	public static void main(String args[]) {
+		
+	}
+}
