b599ed1ce6c899d2ff737ecefadad93217bc38f1
mei <meiapr10@gmail.com>
2022-12-27 10:16:59 +0530

    commit of wrong page

--- src/main/java/com/cart/pages/Orderpage.java
+++ src/main/java/com/cart/pages/Orderpage.java
@@ -4,16 +4,10 @@ public class Orderpage {
 
 
 	public void makeOrder() {
-<<<<<<< HEAD
 		System.out.println("order page...");
 		System.out.println("order is in progrss...");
-=======
-		System.out.println("order page....");
-		System.out.println("order is in progress");
-		
 	}
 	public static void main(String args[]) {
-		
->>>>>>> 430a4c87c0b812c3fd3a73d0f15d582af17ed40b
+
 	}
 }
